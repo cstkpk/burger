2d12375997dd5571aea06b7ec05b8cb53746c68c
Cara Sunberg <cnsunberg@gmail.com>
2019-07-24 18:49:37 -0400

    Created database schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Portobello mushroom");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black bean", true);
+INSERT INTO burgers (burger_name) VALUES ("Sweet potato");
