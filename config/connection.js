1bcc894371ebc72431ff409479a69951539d23aa
Cara Sunberg <cnsunberg@gmail.com>
2019-07-28 13:06:06 -0400

    Added files to prepare for deployment to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,8 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var keys = require("./keys");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection = mysql.createConnection(keys.mysql);
 
 // Make connection.
 connection.connect(function(err) {
